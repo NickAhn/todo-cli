92c66251a7716be298adacb359d6ae8cbc683f00
Nick Ahn <n_ahn2@u.pacific.edu>
2022-12-25 00:51:20 -0800

    Add del command

---
+++ cmd/del.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/NickAhn/todo/data"
+	"github.com/spf13/cobra"
+)
+
+// delCmd represents the del command
+var delCmd = &cobra.Command{
+	Use:   "del",
+	Short: "Delete a todo item",
+	Long:  `del Deletes a todo item from the list at specified index.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("del called")
+		temp, _ := data.ReadItems("./.test.json")
+		index, _ := strconv.Atoi(args[0])
+		items := append(temp[:index], temp[index+1:]...)
+
+		data.SaveItems("./.test.json", items)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(delCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// delCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// delCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
